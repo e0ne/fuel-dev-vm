4bdf83692708a8a200a2e41d99a63866ea6f2e97
Victor Denisov <denisovenator@gmail.com>
2013-12-30 10:26:24 +0400

    Provision fuel-docs requirements

--- fuel-docs.sh
+++ fuel-docs.sh
@@ -1,1 +1,13 @@
-fuel-docs.sh
+#!/bin/bash
+
+easy_install -U Sphinx
+easy_install -U cloud_sptheme
+easy_install -U sphinxcontrib-fancybox
+easy_install -U rst2pdf
+easy_install -U sphinxcontrib-blockdiag
+easy_install -U sphinxcontrib-actdiag
+easy_install -U sphinxcontrib-seqdiag
+easy_install -U sphinxcontrib-nwdiag
+easy_install -U sphinxcontrib-plantuml
+
+apt-get install imagemagick
